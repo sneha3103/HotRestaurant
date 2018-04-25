3d6dbbe98fe68c38e07ae443647188d0b7ed57e2
Vinanti <vinantinaik@gmail.com>
2018-04-24 19:55:28 -0500

    restaurant customer data

---
+++ app/data/restaurant.js
@@ -0,0 +1,23 @@
+
+
+var Customers = [{
+    customerId:1,
+    customerName:"John",
+    phoneNumber:'281-123-2345',
+    customerEmail:"john@gmail.com"
+},
+{
+    customerId:2,
+    customerName:"Ria",
+    phoneNumber:'281-234-2345',
+    customerEmail:"ria@yahoo.com"
+},
+{
+    customerId:3,
+    customerName:"Rose",
+    phoneNumber:'713-123-2345',
+    customerEmail:"rose@hotmail.com"
+}
+]
+
+var waitingList=[];
