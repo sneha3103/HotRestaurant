6f6fafe8e84a03d59c1078d514aa42b3458d8c29
Sneha Dama <snehadama@Snehas-Air.lan>
2018-04-24 20:53:29 -0500

    Added post method

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,20 +4,26 @@ var restaurantData = require("../data/restaurant.js");
 
 console.log(restaurantData);
 
-module.exports = function(app) {
+module.exports = function (app) {
     console.log("Export worked");
 
 
-//Collect data from server
-app.get("/api/tables", function(req, res) {
-    // res.json("Test Data");
-    res.json(restaurantData.Customers);
-});
+    //Collect data from server
+    app.get("/api/tables", function (req, res) {
+        // res.json("Test Data");
+        res.json(restaurantData.Customers);
+    });
 
 
-app.get("/api/waitlist", function(req, res) {
-    res.json(restaurantData.waitingList);
-});
+    app.get("/api/waitlist", function (req, res) {
+        res.json(restaurantData.waitingList);
+    });
+
+    app.post("/api/tables", function(req, res) {
+        res.json(req.body);
+
+    })
+
 
 };
 
